1137d13eeb6c24f179abc5c4c813f936d078ab82
youexcellabs <datascience.youexcel@gmail.com>
2026-01-06 12:30:42 +0500

    new

---
+++ 6th jan 2026 (WDP).py
@@ -0,0 +1,123 @@
+# # #Syntax Familiarity
+# # 
+# # # 2 types of functions in python!
+# # # 
+# # # - Built-in Functions
+# # # - User Defined Functions
+# # 
+# # 
+# # built-in functions:
+# #     
+# #     print
+# #     input
+# #     def
+# #     if
+# #     else
+# #     elif
+# #     sum
+# #     min
+# #     max
+# #     for
+# #     while
+# #     etc
+# 
+# print #to print / display any statment or value
+# 
+# 
+# data types:
+#     
+#     numeric --
+#     -- int = Whole numbers (0,1,2,3,4,5 ...)
+#     -- float = decimal numbers (1.1, 1.2, etc)
+#     
+#     string = "Rafay", "rafay"
+#     bool = True / False
+#     
+#     arithmetic operands:
+#         + (to add)ex: - 2 + 2
+#         - (to subtract) ex: 2 - 2
+#         / (to divide) ex: 2/2
+#         * (to multiply) ex: 2*2
+#         ** (for exponence) ex: 2**4
+#         // (floor division) ex: 2//2
+#         % (modulus operand) ex: 2%5
+#         
+#     relational operator:
+#         == L.H.S is equal to R.H.S
+#         > L.H.S is greater than R.H.S
+#         < L.H.S is less than R.H.S
+#         >= L.H.S is greater than or equal to R.H.S
+#         <= L.H.S is Less Than or equal to R.H.S
+#         != L.H.S is not equal to R.H.S
+#         
+#     Logical Operators:
+#         and = works when both conditions are true
+#         or = Works when any condition is true 
+#         not = works when both are false
+#         
+#     membership operators:
+#         is
+#         in
+#         
+# 
+# 
+# 
+# 
+# 
+# #
+
+
+
+#print("Hello this is rafay, \nand we're in web dev with python batch")
+
+# escape sequence in python
+# 
+# \n = for new line ex: "\n"
+# \t = for 8 indentations
+
+
+#Variables in python!
+
+# hello=12
+# 
+# print("hello is equal to:",hello)
+
+
+# num1=50
+# num2=100
+# print("This is Addition",num1+num2,"\nThis is subtraction:",num1-num2,)
+#Variable into variables!
+
+# num1=50
+# num2=100
+# add=num1+num2
+# sub=num1-num2
+# div=num1/num2
+# mul=num1*num2
+# 
+# print("This is Addition",add,"\nThis is subtraction:",sub,"\nThis is division:",div,"\nThis is multiplication:",mul)
+#input / output in python!
+
+num1=int(input("Enter a number:")) # "50"
+
+#charc of input, bydefault considers value as string,
+#so we do typecast
+#for the purpose of calculation
+print("This is num1:",num1+num1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
